216a6ab096d31e712384710069e8d1c272b28c30
rleszcz <leszczynski.radoslaw@gmail.com>
2021-04-11 20:35:55 +0200

    unused imports were removed

--- src/test/java/testing/MyBasicTest.java
+++ src/test/java/testing/MyBasicTest.java
@@ -1,13 +1,7 @@
 package testing;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
